243a2ebcbe74c2b0393d66cf40ff11dd1e57792e
Yassin charrouf errynda <ycha0707@g.educaand.es>
2024-05-01 12:35:07 +0200

    Primer commit con netbeans

---
+++ src/javaapplication34/JavaApplication34.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication34;
+
+/**
+ *
+ * @author Yassin
+ */
+public class JavaApplication34 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Esto es el ejemplo para git.");
+        int suma;
+        int num1=2,num2=3;
+        suma=num1+num2;
+        System.out.println("resultado:"+suma);
+    }
+    
+}
